d2bcb4518af78c4ca9e106c7736093b9600568ce
Vishal Alkari <vishalalkari999@gmail.com>
2023-03-27 00:16:49 +0530

    Add Dropdown component with useRef hook.

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect, useRef } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
+
+export default function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) return;
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler, true);
+    };
+  }, []);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onChange(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        onClick={handleClick}
+        className="flex justify-between items-center cursor-pointer"
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
+    </div>
+  );
+}
